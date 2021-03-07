4e46e492b5d9313a4ec2376c499105bce78f630d
Osmar Junior <oj13black@hotmail.com>
2021-03-07 13:16:34 -0300

    init

---
+++ Udemy/Iniciantes/inicio.py
@@ -0,0 +1,28 @@
+a = 1
+b = 2
+c = 3
+
+d = a + b + c
+
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+valorHora=5
+dias=30
+horaTrabalho=8
+salario=valorHora*dias*horaTrabalho
+nome="Osmar Junior"
+
+print(valorHora)
+print(dias)
+print(horaTrabalho)
+print(salario)
+print(nome)
+
+print(float(valorHora))
+print(int(15.8))
+print(str(8000))
+
+
