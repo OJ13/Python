199c540752d3331eb731ea3c950c4a9db5c4151c
Osmar Junior <oj13black@hotmail.com>
2021-03-18 09:17:12 -0300

    while, pass, continue, break

--- Udemy/Iniciantes/loops.py
+++ Udemy/Iniciantes/loops.py
@@ -17,3 +17,32 @@ print("------------")
 lista=["Rocky", 1999, "Balboa"]
 for valor in lista:
     print(valor)
+
+print("-------------")
+numero = 15
+while (numero > 0):
+    print("numero: ", numero)
+    numero = numero - 1
+
+print("------------")
+numero = 20
+while True:
+    numero = numero -1
+    print("numero é: ", numero)
+    if (numero == 2):
+        break
+
+numero = 10
+while True:
+    numero =  numero - 1
+    if (numero == 4):
+        print("passou no continue")
+        continue
+    print(numero)
+    if (numero == 2):
+        break
+
+print("--------------")
+for x in range(0,5):
+    pass
+    
