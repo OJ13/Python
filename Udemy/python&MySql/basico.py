50350cc07380dbe5809c2661ffaf8efe88ce606b
Osmar Junior <oj13black@hotmail.com>
2021-04-01 09:07:02 -0300

    basic

---
+++ Udemy/python&MySql/basico.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+#primeiro programa
+print("Hello World")
+print("Quarta feira de manha")
+print("mensagem 3")
+print("Olá Mundo!")
+
+"""
+Comentario
+de
+ multiplas 
+
+ linhas
+"""
+### funções matemáticas
+print(2 + 2)
+print(5 - 3)
+print(2 / 2)
+print(5 * 5)
+print(2 **3) # 2 elevado a 3
+print(10 / 3)
+print(10 % 3)
+
+### variaveis e dados
+var1 = 1        #int
+var2 = 5.58     #float
+var3 = "OJ"     #string
+var4 = True     #boolean
+
+### Operadores lógicos
+x = 2
+y = 3
+
+if(x == y):
+    print("igual")
+
+if(x != y):
+    print("difente")
+
+if(x < y):
+    print("x é menor que y")
+
+if(x > y):
+    print("x é maior que y")
+
+if(x <= y):
+    print("x é menor ou igual que y")
+
+if(x >= y):
+    print("x é maior ou igual que y")
+
+if(x < y and x != y):
+    print("And")
+
+if(x < y or x == y):
+    print("Or")
+
+if(not x == y):
+    print("NOT")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
