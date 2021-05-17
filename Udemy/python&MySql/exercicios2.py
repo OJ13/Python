1e1bb302a8abb7de884a214d32359e3284d82008
Osmar Junior <oj13black@hotmail.com>
2021-05-17 09:05:07 -0300

    finish exercise

--- Udemy/python&MySql/exercicios2.py
+++ Udemy/python&MySql/exercicios2.py
@@ -1,4 +1,4 @@
-### Comparando Sequencias
+## Comparando Sequencias
 import re
 seq = input("Digite a sequencia 1: ")
 seq2 = input("Digite a sequencia 2: ")
@@ -33,6 +33,33 @@ arquivo.write(sequencia)
 print("Sequencia inserida com sucesso")
 
 
+##----------------------
+## Selecionando o Menu
+
+menu = 0
+
+def abrirArquivo():
+    nome = input("Digite o nome do arquivo que deseja abrir: ")
+    arquivo = open(nome)
+
+    return arquivo
+
+def lerArquivo(arquivo):
+    linhas = arquivo.readlines()
+
+    for linha in linhas:
+        print(linha.strip())
+
+
+while menu != 3:
+    print(" 1 - Abrir Arquivo \n 2 - Ler Arquivo Aberto \n 3 - Sair")
+    
+    menu = int(input("Digite a opção desejada: "))
+
+    if menu == 1:
+        arquivo = abrirArquivo()
+    elif menu == 2:
+        lerArquivo(arquivo)
 
 
 
